8cb3102f079b2bde646f1fd32cd10ff1bbb28358
ny-filip-k <226062935+ny-filip-k@users.noreply.github.com>
2026-02-10 09:58:14 +0100

    add multiple pictures

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ document.documentElement.style.setProperty(
 async function gambling(caseName) {
   let hugeHardCoding = Array.from({ length: 100 });
   let itemCount = 3;
+  let velocity = 1;
+  let position = 0;
+  let subCoordinate = 0;
+
+
 
   for (let i = 0; i < 100; i++) {
     let randomNumber = Math.floor(Math.random() * itemCount) + 1;
@@ -16,28 +21,38 @@ async function gambling(caseName) {
   }
 
 
-  let velocity = 1;
-  let position = 0;
-
   for (let i = 0; i < 20; i++) {
     position += velocity;
     velocity -= 0.01;
-    console.log(`velocity ${Math.round(velocity)}`);
+    //console.log(`velocity ${Math.round(velocity)}`);
     console.log(`position ${Math.round(position)}`);
 
+    subCoordinate = position - Math.floor(position);
+    console.log(subCoordinate);
+
     item = hugeHardCoding[Math.round(position)];
+    itemPrevious = hugeHardCoding[Math.round(position - 1)];
+    itemNext = hugeHardCoding[Math.round(position + 1)];
 
     if (velocity < 0) {
       velocity = 0;
     }
-    console.log(`item ${hugeHardCoding[Math.round(position)]}`)
+    //console.log(`item ${hugeHardCoding[Math.round(position)]}`)
 
 
-    let gamblingPicture = `${caseName}${item}.png`;
+    let gamblingPicture = `/images/${caseName}${item}.png`;
+    let gamblingPicturePrevious = `/images/${caseName}${itemPrevious}.png`;
+    let gamblingPictureNext = `/images/${caseName}${itemNext}.png`;
+
+    document.getElementById("gamblingPrevious").src = gamblingPicturePrevious;
     document.getElementById("gamblingCurrent").src = gamblingPicture;
+    document.getElementById("gamblingNext").src = gamblingPictureNext;
 
     await sleep(100);
   }
+
+
+
   console.log(`${position} final`)
   return position;
 }
